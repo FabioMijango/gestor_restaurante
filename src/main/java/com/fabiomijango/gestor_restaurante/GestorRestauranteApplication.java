1fe63803036e03d3d7e2b9ddf7712bd570d60d2f
FabioMijango <00156022@uca.edu.sv>
2025-10-13 11:55:42 -0600

    Add main application class for Spring Boot setup

---
+++ src/main/java/com/fabiomijango/gestor_restaurante/GestorRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.fabiomijango.gestor_restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorRestauranteApplication.class, args);
+	}
+
+}

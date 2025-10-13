0d0709a27cef48236e08ea948a417fcb5f458e3f
FabioMijango <00156022@uca.edu.sv>
2025-10-13 11:57:24 -0600

    Add unit test file (Not in use)

---
+++ src/test/java/com/fabiomijango/gestor_restaurante/GestorRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiomijango.gestor_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d5cdfac5090dc466bb084a37f032ee210bea517a
FabioMijango <00156022@uca.edu.sv>
2025-10-13 12:00:40 -0600

    Add initial SQL data for dish categories, table states, order states, and user roles

---
+++ src/main/resources/import.sql
@@ -0,0 +1,44 @@
+INSERT INTO dish_categories
+VALUES (1, 'Main Dish');
+
+INSERT INTO dish_categories
+VALUES (2, 'Drink');
+
+INSERT INTO dish_categories
+VALUES (3, 'Dessert');
+
+INSERT INTO table_states
+VALUES (1, 'Available');
+
+INSERT INTO table_states
+VALUES (2, 'Reserved');
+
+INSERT INTO table_states
+VALUES (3, 'Occupied');
+
+INSERT INTO order_states
+VALUES (1, 'Pending');
+
+INSERT INTO order_states
+VALUES (2, 'In Preparation');
+
+INSERT INTO order_states
+VALUES (3, 'Ready');
+
+INSERT INTO order_states
+VALUES (4, 'Served');
+
+INSERT INTO order_states
+VALUES (5, 'Cancelled');
+
+INSERT INTO order_states
+VALUES (6, 'Paid');
+
+INSERT INTO user_roles
+VALUES (1, 'Admin');
+
+INSERT INTO user_roles
+VALUES (2, 'Waiter');
+
+INSERT INTO user_roles
+VALUES (3, 'Chef');

6c8954d0c428e9203659ac00f1d232ca3e69d562
FabioMijango <00156022@uca.edu.sv>
2025-10-15 13:55:58 -0600

    Add CORS configuration to SecurityConfig for improved API access control

--- src/main/java/com/fabiomijango/gestor_restaurante/configuration/SecurityConfig.java
+++ src/main/java/com/fabiomijango/gestor_restaurante/configuration/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -22,8 +27,22 @@ public class SecurityConfig {
                         auth.anyRequest().permitAll() // FIXME: Cambiar
                 )
                 .csrf( config -> config.disable())
+                .cors( cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement( management ->
                         management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }

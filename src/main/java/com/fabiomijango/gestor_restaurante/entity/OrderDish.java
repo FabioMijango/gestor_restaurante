7082a69956417eb74648a32aeb3a6fb5c7908892
FabioMijango <00156022@uca.edu.sv>
2025-10-18 00:44:10 -0600

    REMOVE cascade option from Order relationship in OrderDish entity

--- src/main/java/com/fabiomijango/gestor_restaurante/entity/OrderDish.java
+++ src/main/java/com/fabiomijango/gestor_restaurante/entity/OrderDish.java
@@ -15,7 +15,7 @@ public class OrderDish {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id", nullable = false)
     private Order order;
 

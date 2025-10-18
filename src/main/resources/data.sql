add26873b1c4ab56c0856c57937453a028392b8f
FabioMijango <00156022@uca.edu.sv>
2025-10-17 23:08:17 -0600

    ADD initial data.sql file with sample dishes for the menu

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+-- Dishes
+
+INSERT INTO dishes (id, description, is_available, name, price, category_id)
+VALUES
+('30f60d00-5f20-4642-8ff0-a0946f9f3f3c', 'Classic Margherita pizza with fresh tomatoes, mozzarella cheese, and basil.', true, 'Margherita Pizza', 8.99, 1),
+('6985f40e-81e0-4f13-93ba-e1879b634929', 'Pepperoni pizza topped with spicy pepperoni slices and mozzarella cheese.', true, 'Pepperoni Pizza', 9.99, 1),
+('a3d7930d-7fcd-480e-a029-3aaa4fed278b', 'Caesar salad with romaine lettuce, croutons, Parmesan cheese, and Caesar dressing.', true, 'Caesar Salad', 6.49, 5),
+('ed42e57e-63e7-487f-9350-f074f46902d8', 'Grilled chicken sandwich with lettuce, tomato, and mayo on a toasted bun.', true, 'Grilled Chicken Sandwich', 7.99, 7),
+('f5958ddb-e471-4e23-a849-f16dc2a96c67', 'Spaghetti Bolognese with rich meat sauce and Parmesan cheese.', true, 'Spaghetti Bolognese', 10.49, 4),
+('54415b6b-c2d0-44cd-902c-a1b3ff6b6037', 'Chocolate lava cake with a molten chocolate center served with vanilla ice cream.', true, 'Chocolate Lava Cake', 5.99, 3),
+('d998742d-a57f-44a0-b164-d46ffa19954c', 'Fresh garden salad with mixed greens, cherry tomatoes, cucumbers, and vinaigrette.', true, 'Garden Salad', 5.49, 5),
+('63bbd3ad-6950-428b-9027-3d2ec7fdf04a', 'BBQ ribs glazed with tangy barbecue sauce served with coleslaw.', true, 'BBQ Ribs', 12.99, 7),
+('e0f243d9-cbce-4b32-afdd-19eff6067575', 'Vegetarian wrap with hummus, grilled vegetables, and fresh greens.', true, 'Vegetarian Wrap', 7.49, 4),
+('10c89430-73cf-4a5b-9b3a-e5568dd26280', 'Classic cheeseburger with lettuce, tomato, pickles, and cheddar cheese.', true, 'Cheeseburger', 8.49, 1);
+
+
